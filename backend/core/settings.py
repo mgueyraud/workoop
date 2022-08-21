33a35bcf227575e4d30203148fbbb41bfaf55416
mgueyraud <mgueyraud.junior@gmail.com>
2022-08-21 19:07:52 -0400

    Se agrega soporte para todos los hosts backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-x@yj1iw#w%^@boo^p&dmcbb!l0+skfn==8rw1q1*^23**yzw(9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['backend', 'localhost', 'workoop-backend.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
