ca85cc3c8c51cb49d04918d3f4c1c849aa2b589a
mgueyraud <mgueyraud.junior@gmail.com>
2022-08-22 08:55:32 -0400

    Se pone bien el BASE_DIR

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,12 +16,11 @@ import django_heroku
 import environ
 
 
-env = environ.Env()
-environ.Env.read_env(str(BASE_DIR) + '/.env')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(str(BASE_DIR) + '/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
